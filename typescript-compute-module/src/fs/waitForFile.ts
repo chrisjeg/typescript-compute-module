ef5a76bc4ac0d207d2861f5a8a4fc08bbc6a1317
chrisjeg <4092952+chrisjeg@users.noreply.github.com>
2024-09-05 10:36:22 +0100

    Add files

--- typescript-compute-module/src/fs/waitForFile.ts
+++ typescript-compute-module/src/fs/waitForFile.ts
@@ -7,7 +7,6 @@ export function waitForJsonFile<T>(filePath: string): Promise<T> {
 
 export function waitForFile(filePath: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    // Function to read the file and resolve the promise
     const readFileAndResolve = () => {
       fs.readFile(filePath, "utf8", (err, data) => {
         if (err) {
@@ -20,23 +19,19 @@ export function waitForFile(filePath: string): Promise<string> {
     // Check if the file already exists
     fs.access(filePath, fs.constants.F_OK, (err) => {
       if (!err) {
-        // File exists, read it immediately
         return readFileAndResolve();
       }
 
-      // File does not exist, set up a watcher
       const directory = path.dirname(filePath);
       const fileName = path.basename(filePath);
 
       const watcher = fs.watch(directory, (eventType, changedFileName) => {
         if (eventType === "rename" && changedFileName === fileName) {
-          // Stop watching the directory
           watcher.close();
-          // Read and resolve the file content
           readFileAndResolve();
         }
       });
-      // Handle errors in watching
+
       watcher.on("error", (err) => {
         watcher.close();
         reject(err);
